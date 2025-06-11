30163886793d021ae1561bbf8dfd0f67fea1c26e
Jesse Mcleod <irontom10@gmail.com>
2025-06-11 19:37:02 -0400

    added the rest of the armors back need to figure out what a armor trim is and finish the effects and enchants

---
+++ src/main/java/com/irontom10/freespawn/item/armor/amethyst/AmethystHelmet.java
@@ -0,0 +1,56 @@
+package com.irontom10.freespawn.item.armor.amethyst;
+
+
+import com.irontom10.freespawn.item.armor.ModArmorMaterials;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+
+public class AmethystHelmet extends ArmorItem {
+    public AmethystHelmet() {
+        super(
+                ModArmorMaterials.AMETHYST,  // material
+                Type.HELMET,               // armor type
+                new Properties()
+                        .fireResistant()  // properties
+        );
+    }
+
+    @Override
+    public void inventoryTick(
+            ItemStack stack,
+            Level world,
+            net.minecraft.world.entity.Entity entity,
+            int      slot,
+            boolean  selected
+    ) {
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.ALL_DAMAGE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.ALL_DAMAGE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FIRE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.FIRE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.BLAST_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.BLAST_PROTECTION, 5);
+        }
+        if( !world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PROJECTILE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.PROJECTILE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.RESPIRATION, stack) <= 0) {
+            stack.enchant(Enchantments.RESPIRATION, 2);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.AQUA_AFFINITY, stack) <= 0) {
+            stack.enchant(Enchantments.AQUA_AFFINITY, 3);
+        }
+
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+}
---
+++ src/main/java/com/irontom10/freespawn/item/armor/lapis/LapisBoots.java
@@ -0,0 +1,54 @@
+package com.irontom10.freespawn.item.armor.lapis;
+
+import com.irontom10.freespawn.item.armor.ModArmorMaterials;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+
+public class LapisBoots extends ArmorItem {
+    public LapisBoots() {
+        super(
+                ModArmorMaterials.LAPIS,  // material
+                Type.BOOTS,               // armor type
+                new Properties()
+                        .fireResistant()  // properties
+        );
+    }
+
+    @Override
+    public void inventoryTick(
+            ItemStack stack,
+            Level world,
+            net.minecraft.world.entity.Entity entity,
+            int      slot,
+            boolean  selected
+    ) {
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.ALL_DAMAGE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.ALL_DAMAGE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FIRE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.FIRE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.BLAST_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.BLAST_PROTECTION, 5);
+        }
+        if( !world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PROJECTILE_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.PROJECTILE_PROTECTION, 5);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FALL_PROTECTION, stack) <= 0) {
+            stack.enchant(Enchantments.FALL_PROTECTION, 3);
+        }
+        if (!world.isClientSide()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FROST_WALKER, stack) <= 0) {
+            stack.enchant(Enchantments.FROST_WALKER, 3);
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+}
